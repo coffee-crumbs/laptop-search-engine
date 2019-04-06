d5443846e6f49246279ab1b04bc80565ab96196b
ZachStapletonJones <zach.stapletonjones@knowinnovation.com>
2019-04-06 14:25:04 -0400

    Initilized Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAYeN32z4zPwh6xogg81mISTi2n3MJY6s4",
+    authDomain: "laptop-search-engine-77d09.firebaseapp.com",
+    databaseURL: "https://laptop-search-engine-77d09.firebaseio.com",
+    projectId: "laptop-search-engine-77d09",
+    storageBucket: "laptop-search-engine-77d09.appspot.com",
+    messagingSenderId: "934329623534"
+  };
+  firebase.initializeApp(config);
